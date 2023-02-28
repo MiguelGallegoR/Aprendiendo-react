999b985430693244fd1c67c8a6466d930366b793
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-02-28 12:35:14 +0100

    added initialIsFollowing

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -5,7 +5,7 @@ import { TwitterFollowCard } from "./TwitterFollowCard.jsx";
 export function App(){
   return(
     <section className="App">
-      <TwitterFollowCard userName="midudev">
+      <TwitterFollowCard userName="midudev" initialIsFollowing={true}>
         Miguel Ángel Durán
       </TwitterFollowCard>
 
