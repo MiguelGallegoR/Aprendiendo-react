8f9032ce47b7c3bc04491ffd9565dded69688ecd
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-02-28 13:41:35 +0100

    added users array and map component

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -2,16 +2,43 @@ import React from 'react'
 import './App.css'
 import { TwitterFollowCard } from "./TwitterFollowCard.jsx";
 
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'MiguelGallego',
+    name: 'Miguel Gallego Rodríguez',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo H.',
+    isFollowing: false
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false
+  }
+]
+
+
+
+
 export function App(){
   return(
     <section className="App">
-      <TwitterFollowCard userName="midudev" initialIsFollowing={true}>
-        Miguel Ángel Durán
-      </TwitterFollowCard>
-
-      <TwitterFollowCard userName="homer">
-         Homer Simpson
-      </TwitterFollowCard>
+      {
+        users.map(({userName, name , isFollowing})=>(
+          <TwitterFollowCard  key={userName} userName={userName} initialIsFollowing={isFollowing} >
+              {name}
+          </TwitterFollowCard>
+        ))
+      }
       
     </section>
   )
