dcbef335367b2451fa3212f1f3f52e5a93491623
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-03-21 16:43:02 +0100

    created cart hooks and reducers

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -3,19 +3,20 @@ import { Products } from './components/Products'
 import { Header } from './components/Header'
 import { useFilters } from './hooks/useFilters'
 import { Cart } from './components/Cart'
+import { CartProvider } from './context/cart'
 function App () {
   const { filterProducts } = useFilters()
 
   const filteredProducts = filterProducts(initialProducts)
 
   return (
-    <>
-      <main>
-        <Header />
-        <Cart />
-        <Products products={filteredProducts} />
-      </main>
-    </>
+    <CartProvider>
+
+      <Header />
+      <Cart />
+      <Products products={filteredProducts} />
+
+    </CartProvider>
   )
 }
 
--- projects/06-shopping-cart/src/components/Cart.jsx
+++ projects/06-shopping-cart/src/components/Cart.jsx
@@ -2,9 +2,29 @@ import './Cart.css'
 
 import { useId } from 'react'
 import { CartIcon, ClearCartIcon } from './Icons'
+import { useCart } from '../hooks/useCart'
 
+export function CartItem ({ thumbnail, price, title, quantity, addToCart }) {
+  return (
+    <li>
+      <img src={thumbnail} alt={title} />
+      <div>
+        <strong>{title}</strong> - ${price}
+      </div>
+
+      <footer>
+        <small>
+          Qty: {quantity}
+        </small>
+        <button onClick={addToCart}>+</button>
+      </footer>
+
+    </li>
+  )
+}
 export function Cart () {
   const cartCheckboxId = useId()
+  const { cart, addToCart, clearCart } = useCart()
 
   return (
     <>
@@ -14,23 +34,17 @@ export function Cart () {
       <input id={cartCheckboxId} type='checkbox' hidden />
       <aside className='cart'>
         <ul>
-          <li>
-            <img src='' alt='' />
-            <div>
-              <strong>Iphone</strong> - $1499
-            </div>
+          {cart.map(product => (
+            <CartItem
+              key={product.id}
+              addToCart={() => addToCart(product)}
+              {...product}
+            />
+          ))}
 
-            <footer>
-              <small>
-                Qty: 1
-              </small>
-              <button>+</button>
-            </footer>
-
-          </li>
         </ul>
 
-        <button>
+        <button onClick={clearCart}>
           <ClearCartIcon />
         </button>
 
