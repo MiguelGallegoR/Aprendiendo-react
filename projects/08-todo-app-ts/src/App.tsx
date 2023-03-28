154255de2d9ef12b6c31f0bf54e68e27136705bd
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-03-28 21:36:40 +0200

    created to-do project

---
+++ projects/08-todo-app-ts/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+export const App = ():JSX.Element => { 
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>todo mvc</h1>
+    )
+  }
