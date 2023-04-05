6b6923cae11d9d47a10a717fabe0c655052ef406
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-04-05 17:32:54 +0200

    Added TextArea

--- projects/09-google-translate-clone/src/App.tsx
+++ projects/09-google-translate-clone/src/App.tsx
@@ -1,14 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { useStore } from './hooks/useStore'
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col, Button, Stack } from 'react-bootstrap'
 import { AUTO_LANGUAGE } from './constants'
 import { ArrowsIcon } from './components/Icons'
 import { LanguageSelector } from './components/LanguageSelector'
 import { SectionType } from './types.d'
+import { TextArea } from './components/TextArea'
 
 function App () {
-  const {  fromLanguage, toLanguage , interchangeLanguages, setFromLanguage, setToLanguage} = useStore()
+  const { loading,
+    fromLanguage,
+    toLanguage, 
+    result, 
+    fromText, 
+    interchangeLanguages, 
+    setFromLanguage, 
+    setToLanguage, 
+    setFromText, 
+    setResult
+  } = useStore()
+
   return (
     <Container fluid>
       
@@ -21,6 +33,11 @@ function App () {
               value={fromLanguage}
               onChange={setFromLanguage} 
             />
+            <TextArea
+              type={SectionType.From}
+              value={fromText}
+              onChange={setFromText}
+            />            
           </Stack>
         </Col>
 
@@ -36,6 +53,12 @@ function App () {
               type={SectionType.To}
               value={toLanguage}
               onChange={setToLanguage}
+            />
+             <TextArea
+              loading={loading}
+              type={SectionType.To}
+              value={result}
+              onChange={setResult}
             />
           </Stack>
         </Col>
