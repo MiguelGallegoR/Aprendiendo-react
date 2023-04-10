7848ed905089fc836650240e02e941d1091cb631
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-04-10 17:40:31 +0200

    Added new project

---
+++ projects/10-react-redux/src/App.tsx
@@ -0,0 +1,13 @@
+import { ListOfUsers } from './components/ListOfUsers'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Nuestro proyecto con Redux</h1>
+    </div>
+  )
+}
+
+export default App
