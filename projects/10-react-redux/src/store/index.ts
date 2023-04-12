489102ec697cc8c97758c52e4194748b024fa4f6
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-04-12 12:38:00 +0200

    created and added rollbackUser

--- projects/10-react-redux/src/store/index.ts
+++ projects/10-react-redux/src/store/index.ts
@@ -1,5 +1,5 @@
 import { configureStore, type Middleware } from "@reduxjs/toolkit";
-import usersReducer from "./users/slice";
+import usersReducer, { rollbackUser } from "./users/slice";
 import { toast } from 'sonner'
 
 const persistanceLocalStorageMiddleware:Middleware = (store) => (next) => (action) => {
@@ -9,20 +9,29 @@ const persistanceLocalStorageMiddleware:Middleware = (store) => (next) => (actio
 
 const syncWithDatabaseMiddleware: Middleware = store => next => action => {
     const { type, payload} = action
-
+    const previousState = store.getState()
     next(action)
 
     if(type === 'users/deleteUserById'){
-        fetch(`https://jsonplaceholder.typicode.com/users/${payload}`,{
+        const userIdToRemove = payload
+        const userToRemove = previousState.users.find(user => user.id === userIdToRemove)
+
+        fetch(`https://jsonplaceholder.typicode.com/users/${userIdToRemove}`,{
             method: 'DELETE'
         })
 
         .then(res => {
-            if(res.ok){
+            /*if(res.ok){
                 toast.success(`Usuario ${payload} eliminado correctamente`)
             }
+            */
+            throw new Error('Error al eliminar usuario')
+
+            
         })
         .catch(err =>{
+            toast.error(`Error deleting user ${userIdToRemove}`)
+            if(userToRemove) store.dispatch(rollbackUser(userToRemove))
             console.log(err)
         })
 
