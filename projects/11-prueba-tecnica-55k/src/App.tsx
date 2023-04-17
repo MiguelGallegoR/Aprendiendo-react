26f5c5e84bdcebb37498c82f417527279a3121af
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-04-17 16:26:57 +0200

    added toggleColors, deleteUser, handleReset

--- projects/11-prueba-tecnica-55k/src/App.tsx
+++ projects/11-prueba-tecnica-55k/src/App.tsx
@@ -1,24 +1,75 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { type User } from './types.d'
 import './App.css'
 import { UsersList } from './components/UsersList'
 
 function App() {
   const [users, setUsers] = useState<User[]>([])
+  const [showColors, setShowColors] = useState(false)
+  const [sortByCountry, setSortByCountry] = useState(false)
+  const [filterCountry, setFilterCountry] = useState<string|null>(null)
+
+  const originalsUsers = useRef<User[]>([])
+  //useRef guarda un valor que queremos que se comparta entre renderizados
+  //pero que al cambiar no se vuelva a renderizar el componente
+  const toggleColors = () => {
+    setShowColors(!showColors)
+  }
+
+  const toggleSortByCountry = () => {
+    setSortByCountry(prevState => !prevState)
+  }
+
+  const handleDelete = (email: string) => {
+      const filteredUsers = users.filter(user => user.email !== email)
+      setUsers(filteredUsers)
+  }
+
+  const handleReset = () => {
+      setUsers(originalsUsers.current)
+  }
+
   useEffect(() => {
     fetch('https://randomuser.me/api?results=100')
       .then(res => res.json())
       .then(res => {
         setUsers(res.results)
+        originalsUsers.current = res.results
       })
       .catch(err => {
         console.error()
       })
   }, [])
+
+  const sortedUsers = sortByCountry 
+    ? users.toSorted((a, b) => {
+      return a.location.country.localeCompare(b.location.country)
+    })
+    : users
+
   return (
     <div className="App">
-        <h1>Prueba Técnica</h1>
-        <UsersList users={users} />
+      <h1>Prueba Técnica</h1>
+      <header> 
+        <button onClick={toggleColors}>
+          Colorear filas
+        </button>
+
+        <button onClick={toggleSortByCountry}>
+         { sortByCountry ? 'No ordenar por país' : 'Ordenar por país' }
+        </button>
+
+        <button onClick={handleReset}>
+          Resetear Usuarios 
+        </button>
+
+        <input placeholder='FIltra por país' onChange={(e) => {
+          setFilterCountry(e.target.value)
+        }} />
+      </header>
+      <main>
+        <UsersList deleteUser={handleDelete} showColors={showColors} users={sortedUsers} />
+      </main>
     </div>
   )
 }
--- projects/11-prueba-tecnica-55k/src/components/UsersList.tsx
+++ projects/11-prueba-tecnica-55k/src/components/UsersList.tsx
@@ -1,8 +1,10 @@
 import { type User } from "../types.d"
 interface Props{
+    deleteUser: (email: string) => void 
+    showColors: boolean
     users: User[]
 }
-export function UsersList ({ users }:Props) {
+export function UsersList ({ deleteUser, showColors ,users }:Props) {
     return(
         <table width='100%'>
             <thead>
@@ -16,9 +18,11 @@ export function UsersList ({ users }:Props) {
             </thead>
             <tbody>
                 {
-                    users.map(user => {
+                    users.map((user, index) => {
+                        const backgroundColor = index % 2 == 0 ? '#333' : '#555'
+                        const color = showColors ? backgroundColor : 'transparent'
                         return(
-                            <tr key={user.id.value}>
+                            <tr key={user.email} style={{backgroundColor: color}}>
                                 <td>
                                     <img src={user.picture.thumbnail} alt="" />
                                 </td>
@@ -32,7 +36,9 @@ export function UsersList ({ users }:Props) {
                                     {user.location.country}
                                 </td>
                                 <td>
-                                    <button>Eliminar</button>
+                                    <button onClick={()=>{deleteUser(user.email)}}>
+                                        Eliminar
+                                    </button>
                                 </td>
                             </tr>
                         )
