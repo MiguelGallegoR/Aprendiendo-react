caaa230ada28fcf5fdc9e69e615e6b45f0de6a57
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-04-26 17:08:01 +0200

    added react query

--- projects/11-prueba-tecnica-55k/src/App.tsx
+++ projects/11-prueba-tecnica-55k/src/App.tsx
@@ -2,14 +2,28 @@ import { useEffect, useState, useRef, useMemo } from 'react'
 import { type User } from './types.d'
 import './App.css'
 import { UsersList } from './components/UsersList'
+import { useQuery } from '@tanstack/react-query'
 
+const fetchUsers = async (currentPage: number) => {
+  return await fetch(`https://randomuser.me/api?results=10&seed=miguel&page=${currentPage}`)
+      .then(res =>{ 
+        if(!res.ok) throw new Error('Error en la petición')
+        return res.json()
+      })
+      .then(res => res.results)
+}
 function App() {
-  const [users, setUsers] = useState<User[]>([])
+  const {isLoading, isError, data: users = [], refetch} = useQuery<User[]>(
+    ['users'],
+    async () => await fetchUsers(1)
+  )
   const [showColors, setShowColors] = useState(false)
   const [sortByCountry, setSortByCountry] = useState(false)
   const [filterCountry, setFilterCountry] = useState<string|null>(null)
+  const [currentPage, setCurrentPage] = useState(1)
 
-  const originalsUsers = useRef<User[]>([])
+
+
   //useRef guarda un valor que queremos que se comparta entre renderizados
   //pero que al cambiar no se vuelva a renderizar el componente
   const toggleColors = () => {
@@ -21,26 +35,15 @@ function App() {
   }
 
   const handleDelete = (email: string) => {
-      const filteredUsers = users.filter(user => user.email !== email)
-      setUsers(filteredUsers)
+      //const filteredUsers = users.filter(user => user.email !== email)
+      //setUsers(filteredUsers)
   }
 
-  const handleReset = () => {
-      setUsers(originalsUsers.current)
+  const handleReset = async() => {
+      await refetch()
   }
 
-  useEffect(() => {
-    fetch('https://randomuser.me/api?results=10')
-      .then(res => res.json())
-      .then(res => {
-        setUsers(res.results)
-        originalsUsers.current = res.results
-      })
-      .catch(err => {
-        console.error()
-      })
-  }, [])
-
+  
   const filteredUsers = useMemo(() => {
 
     return typeof filterCountry  === 'string' && filterCountry.length > 0
@@ -83,7 +86,14 @@ function App() {
         }} />
       </header>
       <main>
-        <UsersList deleteUser={handleDelete} showColors={showColors} users={sortedUsers} />
+        {users.length > 0  && 
+          <UsersList deleteUser={handleDelete} showColors={showColors} users={sortedUsers} />
+        }
+        {isLoading && <strong>Cargando...</strong>}
+        {isError && <p>Ha habido un error</p>}
+        { !isError && users.length === 0  && <p>No hay usuarios</p>}
+        
+        {!isLoading && !isError && <button onClick={()=>{setCurrentPage(currentPage+1)}}>Cargar más resultados</button>}
       </main>
     </div>
   )
