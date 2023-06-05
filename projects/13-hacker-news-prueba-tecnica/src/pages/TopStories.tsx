9aa4e3c6b13f46dfd3ab885661b2818e6a5421de
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-06-05 15:43:44 +0200

    created TopStories Page

---
+++ projects/13-hacker-news-prueba-tecnica/src/pages/TopStories.tsx
@@ -0,0 +1,18 @@
+import useSWR from 'swr'
+import { getTopStories } from '../services/hacker-news'
+import { Story } from '../components/Story'
+export default function TopStoriesPage() {
+    const { data, isLoading } = useSWR('stories', ()=>getTopStories(1,3))
+    return(
+        <>
+            <ul>
+                {isLoading && <li>Loading...</li>}
+                {data?.map((id: number, index: number) =>(
+                    <li key={id}>
+                        <Story id={id} index={index} />
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
+}
