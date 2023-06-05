86a7703ea0ab7869fde30b8c4d527abbbb267b2e
MiguelGallegoR <gallegorodriguezmiguel58@gmail.com>
2023-06-05 15:42:29 +0200

    created lazy loading

--- projects/13-hacker-news-prueba-tecnica/src/App.tsx
+++ projects/13-hacker-news-prueba-tecnica/src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
 import { Header } from './components/Header'
+import { Route } from 'wouter'
+import { lazy, Suspense } from 'react'
+
+
+const TopStoriesPage = lazy(()=> import('./pages/TopStories'))
+const DetailPage = lazy(()=> import('./pages/Detail'))
+
 function App() {
 
   return (
     <>
       <Header />
-      <h2>Hello World!</h2>
+      <main>
+          <Suspense fallback='Loading...'>
+            <Route path='/' component={TopStoriesPage} />
+            <Route path='/article/:id' component={DetailPage} />
+          </Suspense>
+      </main>
+      
     </>
   )
 }
